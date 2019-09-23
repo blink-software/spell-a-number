ec5a2610a7a792b11a34690ff063e7d924a73cdc
Bartosz Gordon <bartosz.gordon@sealcode.org>
2019-09-23 11:55:54 +0200

    Adjust cli tests to match the new binary name

--- test/cli.test.js
+++ test/cli.test.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const test = require('ava');
 
 test.cb('it should print number spelling for the provided language (pl)', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js -l pl 215', (err, stdout) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js -l pl 215', (err, stdout) => {
 		t.is(stdout, 'dwieście piętnaście\n');
 
 		t.end();
@@ -10,7 +10,7 @@ test.cb('it should print number spelling for the provided language (pl)', t => {
 });
 
 test.cb('it should print number spelling for the provided language (en)', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js -l en 215', (err, stdout) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js -l en 215', (err, stdout) => {
 		t.is(stdout, 'two hundred fifteen\n');
 
 		t.end();
@@ -18,7 +18,7 @@ test.cb('it should print number spelling for the provided language (en)', t => {
 });
 
 test.cb('it should print number spelling in english if language not provided', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js 215', (err, stdout) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js 215', (err, stdout) => {
 		t.is(stdout, 'two hundred fifteen\n');
 
 		t.end();
@@ -26,7 +26,7 @@ test.cb('it should print number spelling in english if language not provided', t
 });
 
 test.cb('it should print an unsupported language error message and help', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js -l xxx 215', (err, stdout, stderr) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js -l xxx 215', (err, stdout, stderr) => {
 		t.is(stderr.includes('Cannot find converter for language: xxx'), true);
 		t.is(stdout.includes('Usage: cli [options] <number>'), true);
 
@@ -35,7 +35,7 @@ test.cb('it should print an unsupported language error message and help', t => {
 });
 
 test.cb('it should print an incorrect number provided error message and help', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js 2asd12', (err, stdout, stderr) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js 2asd12', (err, stdout, stderr) => {
 		t.is(stderr.includes('Incorrect number provided: 2asd12'), true);
 		t.is(stdout.includes('Usage: cli [options] <number>'), true);
 
@@ -44,7 +44,7 @@ test.cb('it should print an incorrect number provided error message and help', t
 });
 
 test.cb('it should print a no number provided error message and help', t => {
-	exec('/usr/lib/node_modules/spellit/cli.js', (err, stdout, stderr) => {
+	exec('/usr/lib/node_modules/spell-a-number/cli.js', (err, stdout, stderr) => {
 		t.is(stderr.includes('Pass a number to spell'), true);
 		t.is(stdout.includes('Usage: cli [options] <number>'), true);
 
